0f31f1f2ce87ca45fb9f01e6b2ebe2eeb98e1915
Ben Miller <bmiller@bedrockbusinessmedia.com>
2015-02-22 10:56:28 -0500

    Hopefully this works!

--- ui.R
+++ ui.R
@@ -9,20 +9,21 @@ shinyUI(ui = pageWithSidebar(
         checkboxGroupInput(inputId = 'variables', label = 'Choose Predictors',
                            choices = c('Lag1', 'Lag2', 'Lag3', 'Lag4', 'Lag5'),
                            selected = c('Lag1', 'Lag2', 'Lag3', 'Lag4', 'Lag5')),
-        sliderInput('threshold', 'Choose Probability Threshold',value = 0.5,
-                    min = 0, max = 1, step = 0.01,),
+        sliderInput('threshold', 'Choose Probability Threshold  (LR, LDA and QDA only)',value = 0.5,
+                    min = 0.4, max = 0.6, step = 0.01,),
         sliderInput('k',
-                    'Choose Number of Nearest Neighbors',value = 5,
+                    'Choose Number of Nearest Neighbors (KNN only)',value = 5,
                     min = 0, max = 20, step = 1,),
         submitButton('Run Prediction')
         ),
     
     mainPanel(
-        p('Prediction Model'), textOutput('model_input'),
-        p('Predictors'), textOutput('variables'),
-        p('Probability Threshold'), textOutput('threshold'),
+        p('Pairwise Plot of Selected Predictors'),
         plotOutput('pairs'),
         p('2010 Weekly Prediction Results'), verbatimTextOutput('results'),
-        p('Model Summary'), verbatimTextOutput('summary')
+        p('Model Summary'), verbatimTextOutput('summary'),
+        p('Prediction Model'), textOutput('model_input'),
+        p('Predictors'), textOutput('variables'),
+        p('Probability Threshold'), textOutput('threshold')
     ))
 )
